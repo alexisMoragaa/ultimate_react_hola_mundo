9f1fe50b712a03c1ab540a824e2c2ad81f94641e
Alexis Moraga <alexis.moraga.gallardo@gmail.com>
2024-11-12 11:09:57 -0300

    Se agregan rutas y componente Nav

---
+++ src/pages/Home.tsx
@@ -0,0 +1,39 @@
+import NavBar from "../components/NavBar/NavBar";
+
+export default function Home() {
+  return (
+    <>
+      <NavBar />
+      <p>Esta es la pagina de home</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa dolores ut
+        tenetur quaerat, repellat blanditiis voluptas deleniti temporibus. Aut
+        dolores deserunt incidunt maxime reprehenderit eius quis amet quidem id
+        earum!
+      </p>
+      <p>
+        Maxime sit cupiditate amet alias. Voluptatum amet id quod laborum vero,
+        accusantium exercitationem quo corrupti. Debitis voluptatem similique
+        necessitatibus, odit quaerat officiis quibusdam quam vel, a recusandae
+        esse maiores voluptatibus?
+      </p>
+      <p>
+        Perferendis qui deleniti illo aliquam impedit excepturi officia eligendi
+        accusamus molestias quis consectetur, distinctio explicabo nam nisi eum
+        reprehenderit totam, incidunt, aut labore fugit minima vel voluptatum
+        mollitia quo! Numquam?
+      </p>
+      <p>
+        Totam vel beatae ducimus vitae magnam odit et quo iure tempora, placeat
+        architecto distinctio, rem animi cupiditate aut modi quam quas quibusdam
+        cumque ipsum dignissimos incidunt? Ipsa nulla vel explicabo?
+      </p>
+      <p>
+        Fuga alias eum dolores laudantium adipisci hic, dignissimos, labore
+        rerum facilis voluptates possimus et saepe maxime accusamus dolore
+        ducimus sit enim quidem unde recusandae inventore pariatur provident!
+        Quam, sit optio?
+      </p>
+    </>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,22 @@
 import { createBrowserRouter } from "react-router-dom";
 import FindMeal from "./FindMeal";
 import ReactQuery from "./ReactQuery";
+import Cms from "./Cms";
+import ContextPage from "./ContextPage";
+import Efects from "./Efects";
+import EjerciciosAnteriores from "./EjerciciosAnteriores";
+import Formularios from "./Formularios";
+import Home from "./Home";
 
 const router = createBrowserRouter([
-  { path: "/", element: <>Hola</> },
+  { path: "/", element: <Home /> },
   { path: "/react-query", element: <ReactQuery /> },
   { path: "/find-meal", element: <FindMeal /> },
+  { path: "/cms", element: <Cms /> },
+  { path: "/context", element: <ContextPage /> },
+  { path: "/efects", element: <Efects /> },
+  { path: "/ejercicios-anteriores", element: <EjerciciosAnteriores /> },
+  { path: "/formularios", element: <Formularios /> },
 ]);
 
 export default router;
